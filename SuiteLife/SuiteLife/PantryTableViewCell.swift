02cae616445779882852c68004cfbbd94ddab6b9
GiselleSerate <giselle.serate@gmail.com>
2017-06-13 17:03:45 -0700

    Added new subclass of ItemTableViewController and ItemTableViewCell. Next up, add a second subclass.

---
+++ SuiteLife/SuiteLife/PantryTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PantryTableViewCell.swift
+//  SuiteLife
+//
+//  Created by cssummer17 on 6/13/17.
+//  Copyright © 2017 cssummer17. All rights reserved.
+//
+
+import UIKit
+
+class PantryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var selectButton: UISwitch!
+    @IBOutlet weak var nameLabel: UILabel!
+
+
+}
