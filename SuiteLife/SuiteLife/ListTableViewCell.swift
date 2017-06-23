d44b6fb052546f02341de4e30667c0ace0fa0fd7
GiselleSerate <giselle.serate@gmail.com>
2017-06-23 12:08:44 -0700

    The function validatePrice is excesePrice is strange and probably not what you want in the edit permission function. Come back to this. Applies to issue #4.

--- SuiteLife/SuiteLife/ListTableViewCell.swift
+++ SuiteLife/SuiteLife/ListTableViewCell.swift
@@ -102,10 +102,11 @@ class ListTableViewCell: UITableViewCell, UITextFieldDelegate {
         // Edit the attributes in the array.
         item?.name = nameLabel.text ?? ""
         item?.checked = checkbox.value! as! Bool
-        item?.price = Int(String(priceLabel.text!.characters.remove(at: priceLabel.text!.characters.index(of: ".")!))) ?? 0
+        item?.price = cleanPrice(price: textField.text)
+        priceLabel.text = String(format: "%d.%02d", item!.price/10, item!.price%10)
         
         if textField.tag == TextFieldType.name { // Did you just finish editing the name label?
-
+            
             if item?.name == "" && storedText == "" { // You took this text and it was blank and now it's blank again.
                 //Do nothing.
                 
@@ -123,36 +124,79 @@ class ListTableViewCell: UITableViewCell, UITextFieldDelegate {
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         // Limit the price field's allowable characters to be a decimal.
-        if textField.tag == TextFieldType.name {
-            let inverseSet = NSCharacterSet(charactersIn:"0123456789").inverted
+        if textField.tag == TextFieldType.price && string != ""{
             
-            let components = string.components(separatedBy: inverseSet)
+            let inverseSetDot = NSCharacterSet(charactersIn:"0123456789.").inverted
+            
+            let components = string.components(separatedBy: inverseSetDot)
             
             let filtered = components.joined(separator: "")
             
-            if filtered == string {
-                return true
-            } else {
-                if string == "." {
-                    let countdots = textField.text!.components(separatedBy:".").count - 1
-                    if countdots == 0 {
-                        return true
-                    }
-                    else {
-                        if countdots > 0 && string == "." {
-                            return false
-                        } else {
-                            return true
-                        }
-                    }
-                }
-                else {
-                    return false
-                }
+            // String validation written assuming no pasting. Because pasting will break it. This is why I have extra validation in DidEndEditing.
+            
+            if filtered == string { // Typed/pasted string has only numbers or periods.
+                return validatePrice(price: textField.text)
+            }
+            else { // string contains something you should not be able to add to a price, such as ?!*^.
+                return false
             }
         }
         else { // You're looking at the name label. They can edit whatever they want.
             return true
+        }
+    }
+    
+    func cleanPrice(price: String?) -> Int { // Returns an int of cents. Format as you wish.
+        var dollars = 0
+        var cents = 0
+        var centsAdjust = 1 // Variable factor to make sure there are enough cents.
+        
+        if price == nil {
+            return 0
+        }
+        else if !price!.characters.contains(".") { // No periods. Only dollars.
+            dollars = Int(price!)!
+        }
+        else { // Extract dollars and cents from text field.
+            let splitArray = price!.components(separatedBy: ".")
+            dollars = Int(splitArray[0])!
+            cents = Int(splitArray[1])!
+            let centsPlaces = splitArray[1].characters.count // How many places of cents did they give us?
+            if centsPlaces == 0 { // Not enough cents places (0).
+                centsAdjust = 0
+            }
+            else if centsPlaces == 0 { // Not enough cents places (1).
+                centsAdjust = 10
+            }
+            else if centsPlaces == 2 { // Wow, the user actually did it right. Do nothing.
+            }
+            else if centsPlaces > 2 { // Too many cents places.
+                centsAdjust = 1/(10*(centsPlaces-2))
+            }
+            else {
+                fatalError("You have managed to input negative cents. Please stop.")
+            }
+        }
+        
+        return dollars*10 + cents*centsAdjust
+    }
+    
+    func validatePrice(price: String?) -> Bool { // Returns a bool telling you if the price is a good price or not.
+        
+        if price == nil { // I'm returning true on a nil price, which may not be correct.
+            return true
+        }
+        else if !price!.characters.contains(".") { // No periods. Only dollars.
+            return true
+        }
+        else { // Extract dollars and cents from text field.
+            let splitArray = price!.components(separatedBy: ".")
+            if splitArray.count == 2 && splitArray[1].characters.count <= 2 { // Only dollars section and cents section, AND only <= 2 cents places.
+                return true
+            }
+            else {
+                return false
+            }
         }
     }
 }
