7ca61ec2711050b39ddb04422ff4265a699fa141
cole-k <colekurashige@gmail.com>
2017-06-26 14:32:47 -0700

    Fix TabMan centering issue (#14), identify cause of issue #17

--- SuiteLife/SuiteLife/ListPantryTabViewController.swift
+++ SuiteLife/SuiteLife/ListPantryTabViewController.swift
@@ -13,7 +13,7 @@ import PureLayout
 
 class ListPantryTabViewController: TabmanViewController, PageboyViewControllerDataSource {
 
-    // Woo global variables.
+    var tabViewControllers: [UIViewController]?
     
     override func viewDidLoad() {
         print("CALLED VIEWDIDLOAD")
@@ -21,6 +21,50 @@ class ListPantryTabViewController: TabmanViewController, PageboyViewControllerDa
         
         // Tabman setup
         
+        self.dataSource = self
+        self.bar.location = .bottom
+        
+        recalculateBarLocation()
+    }
+    
+    
+    // Runs when the device rotates.
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        print("DEVICE ROTATED")
+        coordinator.animate(alongsideTransition: nil) { _ in
+            // Run viewDidLoad to recalibrate the screensize.
+            self.viewDidLoad()
+        }
+    }
+
+    //MARK: Tabman
+    func viewControllers(forPageboyViewController pageboyViewController: PageboyViewController) -> [UIViewController]? {
+        if let viewCons = self.tabViewControllers {
+            // If the view controllers already exist, return them
+            print("returning view cons")
+            return viewCons
+        } else {
+            // Create the view controllers and add them to an array
+            let viewCon1 = self.newViewController(name: "List")
+            let viewCon2 = self.newViewController(name: "Pantry")
+            self.bar.items = [TabmanBar.Item(title: "List"), TabmanBar.Item(title: "Pantry")]
+            self.tabViewControllers = [viewCon1, viewCon2]
+            return self.tabViewControllers
+        }
+    }
+
+    
+    func defaultPageIndex(forPageboyViewController pageboyViewController: PageboyViewController) -> PageboyViewController.PageIndex? {
+        return .first
+    }
+    
+    //MARK: Helper methods
+    
+    private func newViewController(name: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(name)NavTableViewController")
+    }
+    
+    private func recalculateBarLocation() {
         // Get font sizing.
         let font = UIFont.systemFont(ofSize: 16)
         let fontAttributes = [NSFontAttributeName: font] // it says name, but a UIFont works
@@ -31,44 +75,9 @@ class ListPantryTabViewController: TabmanViewController, PageboyViewControllerDa
         let offset = (view.bounds.maxX-tabmanWidth.width-40)/2
         print("The width of the view is \(view.bounds.maxX)")
         
-        self.dataSource = self
         self.bar.appearance = TabmanBar.Appearance({ (appearance) in
             appearance.layout.edgeInset = offset //TODO: figure out what values based on screen size will center this.
         }) // TODO: reset this when the screen rotates???? I don't even know if I need to do this.
-        
-        self.bar.location = .bottom
-        
-    }
-    
-    
-    // Runs when the device rotates.
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        print("DEVICE ROTATED")
-//        if (ListPantryTabViewController.view.window != nil) {
-            self.viewDidLoad()
-//            super.viewWillTransition(to: size, with: coordinator)
-//        }
-    }
-
-    //MARK: Tabman
-    func viewControllers(forPageboyViewController pageboyViewController: PageboyViewController) -> [UIViewController]? {
-        // Create the view controllers for the Pantry and List tables and order them
-        let viewCon1 = self.newViewController(name: "List")
-        let viewCon2 = self.newViewController(name: "Pantry")
-        let viewControllers = [viewCon1, viewCon2]
-        self.bar.items = [TabmanBar.Item(title: "List"), TabmanBar.Item(title: "Pantry")]
-        return viewControllers
-    }
-
-    
-    func defaultPageIndex(forPageboyViewController pageboyViewController: PageboyViewController) -> PageboyViewController.PageIndex? {
-        return nil
-    }
-    
-    //MARK: Helper methods
-    
-    private func newViewController(name: String) -> UIViewController {
-        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(name)NavTableViewController")
     }
     
 
