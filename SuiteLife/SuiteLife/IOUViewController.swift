0f10bd927788bc5533e153fc91294d504d4a6990
GiselleSerate <giselle.serate@gmail.com>
2017-07-06 15:14:14 -0700

    Display IOUS in IOU tab.

--- SuiteLife/SuiteLife/IOUViewController.swift
+++ SuiteLife/SuiteLife/IOUViewController.swift
@@ -7,15 +7,21 @@
 //
 
 import UIKit
+import Firebase
 
 class IOUViewController: UITableViewController {
     
+    let databaseRef = Database.database().reference()
+    
     // Test users for display purposes.
-    var testUserDB = [UserWithCash(name: "Giselle", handle: "@gserate", balance: 1200), UserWithCash(name: "Cole", handle: "@ckurashige", balance: 362), UserWithCash(name: "Jeni", handle: "@jzhu", balance: -869)]
+//    var testUserDB = [UserWithCash(name: "Giselle", handle: "@gserate", balance: 1200), UserWithCash(name: "Cole", handle: "@ckurashige", balance: 362), UserWithCash(name: "Jeni", handle: "@jzhu", balance: -869)]
+
+    var ious: [UserWithCash] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        loadIOUs()
         // Do any additional setup after loading the view.
     }
 
@@ -34,23 +40,51 @@ class IOUViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // The number of rows is equal to the number of items.
-        return testUserDB.count
+        return ious.count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(ious)
         // Dequeue the cell...
         let cellIdentifier = "IOUTableViewCell"
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? IOUTableViewCell
             else {
                 fatalError("The dequeued cell is not an instance of IOUTableViewCell.")
         }
-        
+         
         // Configure the cell...
-        var item = testUserDB[indexPath.row]
+        var item = ious[indexPath.row]
         cell.attachUser(&item)
         cell.controller = self
         return cell
+    }
+    
+    // MARK: Firebase
+    
+    func loadIOUs() { // Loads IOUs into the data structure in this class.
+        databaseRef.child("users/\(Auth.auth().currentUser!.uid)/debts").observeSingleEvent(of: .value, with: {(snapshot) in
+            for child in snapshot.children {
+                if let childRef = child as? DataSnapshot {
+                    self.setDBVals(userID: childRef.key, balance: (childRef.value as? Int)!) // Set each person's balances.
+                }
+            }
+        }) {(error) in
+            print(error.localizedDescription)
+        }
+    }
+    
+    func setDBVals(userID: String, balance: Int) { // Get name and handle from DB and populate ious.
+        databaseRef.child("users/\(userID)").observeSingleEvent(of: .value, with: {(snapshot) in
+            if let childRef = snapshot as? DataSnapshot {
+                let name = childRef.childSnapshot(forPath: "name").value as! String
+                let handle = childRef.childSnapshot(forPath: "handle").value as! String
+                self.ious.append(UserWithCash(name: name, handle: handle, userID: userID, balance: balance))
+            }
+            self.tableView.reloadData()
+        }) {(error) in
+            print(error.localizedDescription)
+        }
     }
 
     /*
--- SuiteLife/SuiteLife/UserWithCash.swift
+++ SuiteLife/SuiteLife/UserWithCash.swift
@@ -7,13 +7,15 @@
 //  For applications of the User class that require some sort of cash information.
 
 import UIKit
+import Firebase
 
 class UserWithCash: User {
+    let databaseRef = Database.database().reference()
     
     var balance: Int // Currently assumes only one singular debt; this will probably change later. oh well
     
-    init(name: String, handle: String, balance: Int) {
+    init(name: String, handle: String, userID: String, balance: Int) {
         self.balance = balance
-        super.init(name: name, handle: handle, userID: "temp debug id")
+        super.init(name: name, handle: handle, userID: userID)
     }
 }
